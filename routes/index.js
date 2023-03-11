5f7865f764ff2da318ca8d16f823ec029bd5e248
Kalvinjay <kalvinjaymendez@gmail.com>
2023-03-11 11:28:07 -0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { route } = require('../app');
+
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 const { check, validationResult } = require('express-validator');
 
 router.get('/', function(req, res){
@@ -19,7 +22,13 @@ router.post('/',
     //console.log(reg.body);
     const errors = validationResult(req);
     if (errors.isEmpty()){
-        res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+        .then(() => {res.send('Thank you for your registration!'); })
+        .catch(() => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+        })
     } else {
         res.render('form',{
             title: 'Registration form',
